2c91acaf22076852c3122f013c39ec8fbff3690d
j2ck1e <j2ck1e86@gmail.com>
2023-11-02 18:19:12 +0300

    Added nav graph

--- app/src/main/java/com/jcdesign/openweatherbottomnavigation/MainActivity.kt
+++ app/src/main/java/com/jcdesign/openweatherbottomnavigation/MainActivity.kt
@@ -1,46 +1,23 @@
 package com.jcdesign.openweatherbottomnavigation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.jcdesign.openweatherbottomnavigation.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        onItemSelect()
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController= findNavController(R.id.fragment)
+
+        bottomNavigationView.setupWithNavController(navController)
 
     }
 
-    private fun onItemSelect() {
-        binding.bNav.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.item1 -> {
-                    Toast.makeText(this, "Item 1", Toast.LENGTH_SHORT).show()
-                }
-
-                R.id.item2 -> {
-                    Toast.makeText(this, "Item 2", Toast.LENGTH_SHORT).show()
-                }
-
-                R.id.item3 -> {
-                    Toast.makeText(this, "Item 3", Toast.LENGTH_SHORT).show()
-                }
-
-                R.id.item4 -> {
-                    Toast.makeText(this, "Item 4", Toast.LENGTH_SHORT).show()
-                }
-
-                R.id.item5 -> {
-                    Toast.makeText(this, "Item 5", Toast.LENGTH_SHORT).show()
-                }
-            }
-            true
-        }
-    }
 }
